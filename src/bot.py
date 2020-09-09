3cad9976fa976da7366401dc301fd4fea9c9dc28
Tappyness1 <ce_zheng_neo@aiap.sg>
2020-09-09 12:13:16 +0800

    bot

--- src/bot.py
+++ src/bot.py
@@ -1,18 +1,13 @@
 from src.inference import load_image, classifier
 from telegram.ext import Updater, MessageHandler, Filters
 import os
-import utils
 from functools import partial
-
-PORT = int(os.environ.get('PORT', 5000))
-TOKEN = os.getenv("TOKEN")
 
 def classify_image(bot, update, util):
     image_file = bot.getFile(update.message.photo[-1].file_id)
     image_file.download("image.jpg")
     pred, prob = classifier("image.jpg")
-    text = util.preds_to_string(preds)
-    update.message.reply_markdown(text)
+    update.message.reply_markdown(pred)
 
 def main():
     TOKEN = os.getenv("TOKEN")
@@ -25,9 +20,8 @@ def main():
 
     dp.add_handler(MessageHandler(Filters.photo, classify_image_callback))
 
-    PORT = int(os.environ.get("PORT", "8443"))
+    PORT = int(os.environ.get("PORT", "5000"))
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
     updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
 
 

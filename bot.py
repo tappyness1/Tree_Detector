61728cba014fd909137880320ebd7b092a8f9f99
Tappyness1 <ce_zheng_neo@aiap.sg>
2020-09-10 08:25:08 +0800

    classifier

--- bot.py
+++ bot.py
@@ -19,13 +19,13 @@ def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+# def help(update, context):
+#     """Send a message when the command /help is issued."""
+#     update.message.reply_text('Help!')
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+# def echo(update, context):
+#     """Echo the user message."""
+#     update.message.reply_text(update.message.text)
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -38,16 +38,16 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    # dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error) 
     
-    # classify_image_callback = partial(classify_image)
-    # dp.add_handler(MessageHandler(Filters.photo, classify_image_callback))
+    classify_image_callback = partial(classify_image)
+    dp.add_handler(MessageHandler(Filters.photo, classify_image_callback))
 
     PORT = int(os.environ.get("PORT", "8443"))
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
